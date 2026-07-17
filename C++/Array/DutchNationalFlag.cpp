79a04ac32fc903f08d383044e496565f19433fa9
Anubhav <anubhavgahlawatcool01@gmail.com>
2026-07-17 14:38:18 +0530

    adding more problems

---
+++ C++/Array/DutchNationalFlag.cpp
@@ -0,0 +1,42 @@
+// this problem is also known as sort 0, 1, 2 problem
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sort012(vector<int>& arr) {
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int mid = 0;
+
+    while(mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high--]);
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = { 0, 1, 2, 0, 1, 2, 0, 1, 2 };
+    sort012(arr);
+
+    for(int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
--- C++/Array/kthElement.cpp
+++ C++/Array/kthElement.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 #include<climits>
 using namespace std;
 
-int kthElement(vector<int>& arr) {
-    
+int kthElement(vector<int>& arr, int k) {
+    sort(arr.begin(), arr.end());
+    return arr[k - 1];
 }
 
 int main() {
-    vector<int> arr = { 1, 4, 3, 2, 6, 5 };
+    vector<int> arr = { 14, 4, 31, 22, 64, 50 };
+    int k = 3;
 
-    vector<int> result = kthElement(arr);
+    int result = kthElement(arr, k);
 
-    for(int i = 0; i < result.size(); i++) {
-        cout << result[i] << " ";
-    }
+    cout << "The " << k << "th smallest element is: " << result << endl;
 
     return 0;
 }
